efabf42369b95365cd2ac98e24e7f660a71e8e8c
xiaozhang <2531219716@qq.com>
2023-09-09 19:12:25 +0800

    吸顶导航的实现

--- src/views/Layout/index.vue
+++ src/views/Layout/index.vue
@@ -2,9 +2,11 @@
 import LayoutNav from './components/LayoutHeader.vue'
 import LayoutHeader from './components/LayoutNav.vue'
 import LayoutFooter from './components/LayoutFooter.vue'
+import LayoutFixed from './components/LayoutFixed.vue'
 </script>
 
 <template>
+  <LayoutFixed />
   <LayoutNav />
   <LayoutHeader />
   <RouterView />
