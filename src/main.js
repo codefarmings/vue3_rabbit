1dad09d4e8e5c22421dd99f9f589c95586f96b26
xiaozhang <2531219716@qq.com>
2023-09-09 15:19:25 +0800

    静态资源的引入和插件

--- src/main.js
+++ src/main.js
@@ -3,6 +3,8 @@ import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 import App from './App.vue'
 import router from './router'
+// 引入初始化样式文件
+import '@/styles/common.scss'
 // 测试
 import {getCategory} from '@/apis/testAPI'
 getCategory().then(res=>{
