c66c8c2bcea6fe33f48ffb653e150369d4588a9e
xiaozhang <2531219716@qq.com>
2023-09-09 16:55:11 +0800

    Layout一级导航的渲染

--- src/views/Layout/index.vue
+++ src/views/Layout/index.vue
@@ -1,6 +1,6 @@
 <script setup>
-import LayoutNav from './components/LayoutNav.vue'
-import LayoutHeader from './components/LayoutHeader.vue'
+import LayoutNav from './components/LayoutHeader.vue'
+import LayoutHeader from './components/LayoutNav.vue'
 import LayoutFooter from './components/LayoutFooter.vue'
 </script>
 
