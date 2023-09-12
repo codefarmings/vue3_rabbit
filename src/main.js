872f97f7a04cd67a72e829db09b2bcc7c71ad1ce
xiaozhang <2531219716@qq.com>
2023-09-12 20:06:45 +0800

    详情页-全局组件化开发-

--- src/main.js
+++ src/main.js
@@ -6,10 +6,12 @@ import router from './router'
 // 引入初始化样式文件
 import '@/styles/common.scss'
 import {lazyPlugin} from "@/directives/index"
+import {componentPlugin} from '@/components'
 
 const app = createApp(App)
 app.use(createPinia())
 app.use(router)
 app.use(lazyPlugin)
+app.use(componentPlugin)
 app.mount('#app')
 
--- src/views/Detail/index.vue
+++ src/views/Detail/index.vue
@@ -3,7 +3,7 @@ import {getDetailAPI} from '@/apis/detail'
 import { ref,onMounted} from 'vue'
 import {useRoute} from 'vue-router'
 import DetailHot from './components/DetailHot.vue'
-import ImageView from '@/components/ImageView.vue'
+
 // 商品详情页
 const goods=ref({})
 const route=useRoute()
@@ -12,6 +12,9 @@ const getGoods=async()=>{
   goods.value=res.result
 }
 onMounted(()=>getGoods())
+const skuChange=(sku)=>{
+  console.log(sku);
+}
 </script>
 
 <template>
@@ -82,7 +85,7 @@ onMounted(()=>getGoods())
                 </dl>
               </div>
               <!-- sku组件 -->
-
+              <XtxSku :goods="goods" @change="skuChange"></XtxSku>
               <!-- 数据组件 -->
 
               <!-- 按钮组件 -->
