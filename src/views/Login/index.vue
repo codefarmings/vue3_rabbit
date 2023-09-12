28077287bae615c636a7081ec734cce774567dac
xiaozhang <2531219716@qq.com>
2023-09-12 22:08:52 +0800

    登录页-表单校验功能的是实现

--- src/views/Login/index.vue
+++ src/views/Login/index.vue
@@ -1,5 +1,25 @@
 <script setup>
+import { ref } from 'vue'
+const form =ref({
+  account:'',
+  password:'',
+  agree:true
+})
+const rules=ref({
+  account:[
+    { required: true, message: '请输入账号', trigger: 'blur' }
+  ],
+  password:[
+    { required: true, message: '请输入密码', trigger: 'blur' },
+    { min: 6, max: 16, message: '长度在 6 到 16 个字符', trigger: 'blur'}
+  ],
+  agree:[{
+    validator:(rule,val,callback)=>{
+      return val?callback():callback(new Error('请勾选协议'))
+    }
+  }]
 
+})
 </script>
 
 
@@ -24,16 +44,16 @@
         </nav>
         <div class="account-box">
           <div class="form">
-            <el-form label-position="right" label-width="60px"
+            <el-form :model="form" :rules="rules" label-position="right" label-width="60px"
               status-icon>
-              <el-form-item  label="账户">
-                <el-input/>
+              <el-form-item prop="account" label="账户">
+                <el-input  v-model="form.account" />
               </el-form-item>
-              <el-form-item label="密码">
-                <el-input/>
+              <el-form-item prop="password" label="密码">
+                <el-input  v-model="form.password"/>
               </el-form-item>
-              <el-form-item label-width="22px">
-                <el-checkbox  size="large">
+              <el-form-item prop="agree" label-width="22px">
+                <el-checkbox v-model="form.agree"  size="large">
                   我已同意隐私条款和服务条款
                 </el-checkbox>
               </el-form-item>
