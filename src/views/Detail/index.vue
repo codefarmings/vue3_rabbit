4c99f1379b54a9d40468a6794112781183786a41
xiaozhang <2531219716@qq.com>
2023-09-12 13:43:05 +0800

    详情页-图片预览组件封装-实现小图切大图的功能

--- src/views/Detail/index.vue
+++ src/views/Detail/index.vue
@@ -3,6 +3,7 @@ import {getDetailAPI} from '@/apis/detail'
 import { ref,onMounted} from 'vue'
 import {useRoute} from 'vue-router'
 import DetailHot from './components/DetailHot.vue'
+import ImageView from '@/components/ImageView.vue'
 // 商品详情页
 const goods=ref({})
 const route=useRoute()
@@ -32,7 +33,7 @@ onMounted(()=>getGoods())
           <div class="goods-info">
             <div class="media">
               <!-- 图片预览区 -->
-
+              <ImageView></ImageView>
               <!-- 统计数量 -->
               <ul class="goods-sales">
                 <li>
