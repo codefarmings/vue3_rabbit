30b7983c0becf67ce6f71e5a3ed05a2685c3b57f
xiaozhang <2531219716@qq.com>
2023-09-16 10:59:55 +0800

    结算-生成订单功能实现

--- src/apis/checkout.js
+++ src/apis/checkout.js
@@ -5,3 +5,12 @@ export function getCheckoutInfoAPI(){
         url:'/member/order/pre'
     })
 }
+
+// 创建订单接口
+export function createOrderAPI(data){
+    return http({
+        url:'/member/order',
+        method:'post',
+        data
+    })
+}
